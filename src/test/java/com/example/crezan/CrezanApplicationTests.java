7240afd78f1b55abae450f8238751c89bf42648a
Harshdeep kaur <72741127+Harshdeep-k01@users.noreply.github.com>
2024-08-25 19:49:12 +0530

    COMMIT

---
+++ src/test/java/com/example/crezan/CrezanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crezan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrezanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
